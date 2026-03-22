751432f15ab9bd3c8d76da4f4ca8c2971a3039ca
apoorvas20 <appy@falconer.com>
2026-03-22 13:54:25 -0700

    deleted more functions

--- File Parsing/FileParsing.py
+++ File Parsing/FileParsing.py
@@ -9,25 +9,7 @@ class Spreadsheet:
         self.entries = [] #to store entries
         self.headers = [] #to store headers of file
     
-    def loadAndParse(self, file_path):
-
-        try:
-            with open(file_path, 'r') as file:
-                # lines = file.readlines()
-
-                #read data and separate headers
-                for line in file:
-                    self.entries.append(line.strip())
-                
-                self.headers.append(self.entries[0])
-
-                self.entries = self.entries[1:]
-            
-                # print(self.headers)
-                # print(self.entries)
-        
-        except Exception as e:
-            print(f"An error occured: {e}")
+    
     
     
 
